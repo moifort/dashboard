505b540df99fe93dacc78e3086ebc27c3852cae0
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-06-19 11:41:39 +0200

    Add indiegogo

--- mozaik/config.js
+++ mozaik/config.js
@@ -45,22 +45,40 @@ var config = {
             columns: 4,
             rows:    4,
             widgets: [
- {
-    type:               'value.value',
-    title:              'KissKissBankBank',
-    url:                'http://crawler:8080/kkbb/collect',
-    postfix:            '€',
-    columns: 2, rows: 2,
-    x: 0, y: 0
+                {
+                    type:               'value.value',
+                    title:              'KissKissBankBank',
+                    url:                'http://crawler:8080/kkbb/collect',
+                    postfix:            '€',
+                    columns: 2, rows: 2,
+                    x: 0, y: 0
                 },
-{
-    type:               'value.value',
-    title:              'KissKissBankBank',
-    url:                'http://crawler:8080/kkbb/backers',
-    postfix:            ' Bakers',
-    columns: 2, rows: 2,
-    x: 2, y: 0
+                {
+                    type:               'value.value',
+                    title:              'KissKissBankBank',
+                    url:                'http://crawler:8080/kkbb/backers',
+                    postfix:            ' Bakers',
+                    columns: 2, rows: 2,
+                    x: 2, y: 0
                 },
+                {
+                    type:               'value.value',
+                    title:              'Indiegogo',
+                    url:                'https://api.indiegogo.com/1/campaigns/1918821.json?api_token=16e63457e7a24c06d39b40b52c0df273098cab82ccd3d4abaafd1a9c7a4edfe7',
+                    pathCurrent:        '$.response.contributions_count',
+                    postfix:            'Bakers',
+                    columns: 2, rows: 2,
+                    x: 0, y: 2
+                },
+                {
+                    type:               'value.value',
+                    title:              'Indiegogo',
+                    url:                'https://api.indiegogo.com/1/campaigns/1918821.json?api_token=16e63457e7a24c06d39b40b52c0df273098cab82ccd3d4abaafd1a9c7a4edfe7',
+                    pathCurrent:        '$.response.collected_funds',
+                    postfix:            ' Bakers',
+                    columns: 2, rows: 2,
+                    x: 2, y: 2
+                }
             ]
         }
     ]
