8971b49b93a52a494dbbed8eb5e4788a9124a324
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-06-30 15:18:34 +0200

    Fix grid display

--- config.js
+++ config.js
@@ -42,8 +42,8 @@ var config = {
         // first dashboard
         {
             // 4 x 3 dashboard
-            columns: 8,
-            rows:    6,
+            columns: 6,
+            rows:    2,
             widgets: [
                 {
                     type:               'value.value',
@@ -51,7 +51,7 @@ var config = {
                     url:                'http://crawler:8080/kkbb/collect',
                     postfix:            ' €',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
+                    columns: 2, rows: 1,
                     x: 0, y: 0
                 },
                 {
@@ -60,8 +60,8 @@ var config = {
                     url:                'http://crawler:8080/kkbb/backers',
                     postfix:            ' Bakers',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
-                    x: 0, y: 2
+                    columns: 2, rows: 1,
+                    x: 0, y: 1
                 },
                 {
                     type:               'value.value',
@@ -69,7 +69,7 @@ var config = {
                     url:                'http://crawler:8080/indiegogo/collect',
                     prefix:            '$ ',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
+                    columns: 2, rows: 1,
                     x: 2, y: 0
                 },
                 {
@@ -78,8 +78,8 @@ var config = {
                     url:                'http://crawler:8080/indiegogo/backers',
                     postfix:            ' Bakers',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
-                    x: 2, y: 2
+                    columns: 2, rows: 1,
+                    x: 2, y: 1
                 },
                 {
                     type:               'value.value',
@@ -87,7 +87,7 @@ var config = {
                     url:                'http://crawler:8080/facebook/favorites',
                     postfix:            ' Likes',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
+                    columns: 2, rows: 1,
                     x: 4, y: 0
                 },
                 {
@@ -96,8 +96,8 @@ var config = {
                     url:                'http://crawler:8080/twitter/favorites',
                     postfix:            ' Likes',
                     lastUpdatedFormat:  'YYYY-MM-DD HH:mm:ss',
-                    columns: 2, rows: 2,
-                    x: 4, y: 2
+                    columns: 2, rows: 1,
+                    x: 4, y: 1
                 }
             ]
         }
